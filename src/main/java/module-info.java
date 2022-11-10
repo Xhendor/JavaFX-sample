1c3d7b5b7ec9b723afab48f27af9dd431a1128ef
xhendor <rosendorsc@gmail.com>
2022-11-10 07:15:08 -0800

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.ejemplo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.ejemplo to javafx.fxml;
+    exports com.example.ejemplo;
+}
